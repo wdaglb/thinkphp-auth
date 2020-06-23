0cc10ce08ed1637fab41a5a7865cd3d528f21793
king east <1207877378@qq.com>
2020-06-23 09:13:55 +0800

    admin_role update'

--- db/migrations/20200416164748_admin_role.php
+++ db/migrations/20200416164748_admin_role.php
@@ -9,7 +9,7 @@ class AdminRole extends AbstractMigration
     {
         $table = $this->table('admin_role', ['comment'=>'用户角色']);
         $table->addColumn('name', 'string', ['limit'=>32, 'comment'=>'角色名']);
-        $table->addColumn('remark', 'string', ['limit'=>255, 'comment'=>'描述']);
+        $table->addColumn('remark', 'string', ['limit'=>255, 'comment'=>'描述', 'default'=>'']);
         $table->addColumn('sort',
             \Phinx\Db\Adapter\MysqlAdapter::PHINX_TYPE_SMALL_INTEGER, [
                 'default'=>0
